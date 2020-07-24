27a19c8e085788afa8c9c91ea8fe3d532889a279
changyanan <changyanan@globalegrow.com>
2020-07-24 18:42:33 +0800

    增加配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,38 @@ module.exports = {
     description: '参考文档',
     // 端口号
     port: 8081,
-    base: '/'
+    base: '/',
+    themeConfig: {
+        nav: [
+            {text: '主页', link: '/', icon: 'reco-home'},
+            {text: '接入指南', link: '/views/jd/'},
+            {text: '加入VIP', link: '/views/bd/'},
+            {text: '良心捐助', link: '/views/wx/'},
+            {text: '支付付款', link: '/views/zfb/'}
+        ],
+        sidebar: 'auto',
+        sidebar:{
+            '/views/jd':[
+                'jd'
+            ],
+            '/views/zfb':[
+                'zfb'
+            ],
+            '/views/wx':[
+                'wx'
+            ],
+            '/views/bd':[
+                'bd'
+            ]
+        }
+    }
+
 }
 
 
 // npm run  docs:dev
 // npm i vuepress-theme-antdocs
 
-
+// :href="$withBase('/imgs/logo.png')"
 // https://gitter.im/xuenan020/community?utm_source=share-link&utm_medium=link&utm_campaign=share-link
 //     [![Gitter](https://badges.gitter.im/xuenan020/community.svg)](https://gitter.im/xuenan020/community?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
